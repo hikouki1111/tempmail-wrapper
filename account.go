2847eac5022278998a90a730143e96d0b1896756
kometarou1145 <97835611+hikouki1111@users.noreply.github.com>
2024-10-18 02:49:26 +0900

    v1.0.2

--- account.go
+++ account.go
@@ -1,102 +1,102 @@
 package tempmail_wrapper
 
 import (
-    "bytes"
-    "encoding/json"
-    "log"
+	"bytes"
+	"encoding/json"
+	"log"
 
-    requests "github.com/RabiesDev/request-helper"
-    "github.com/samber/lo"
-    "github.com/tidwall/gjson"
+	requests "github.com/RabiesDev/request-helper"
+	"github.com/samber/lo"
+	"github.com/tidwall/gjson"
 )
 
 type Account struct {
-    Email string
-    Token string
+	Email string
+	Token string
 }
 
 func NewAccount() (*Account, error) {
-    payload := map[string]interface{}{
-        "max_name_length": 10,
-        "min_name_length": 10,
-    }
-    req := requests.Post(newAccountURL, bytes.NewReader(lo.Must(json.Marshal(payload))))
-    requests.SetHeaders(req, jsonHeader)
-    body, _, err := requests.DoAndReadString(client, req)
+	payload := map[string]interface{}{
+		"max_name_length": 10,
+		"min_name_length": 10,
+	}
+	req := requests.Post(newAccountURL, bytes.NewReader(lo.Must(json.Marshal(payload))))
+	requests.SetHeaders(req, jsonHeader)
+	body, _, err := requests.DoAndReadString(client, req)
 
-    return &Account{
-        Email: gjson.Get(body, "email").String(),
-        Token: gjson.Get(body, "token").String(),
-    }, err
+	return &Account{
+		Email: gjson.Get(body, "email").String(),
+		Token: gjson.Get(body, "token").String(),
+	}, err
 }
 
 func (a *Account) Delete() error {
-    payload := map[string]interface{}{
-        "token": a.Token,
-    }
-    req := requests.Delete(controlURL(a.Email), bytes.NewReader(lo.Must(json.Marshal(payload))))
-    requests.SetHeaders(req, jsonHeader)
-    _, err := requests.Do(client, req)
+	payload := map[string]interface{}{
+		"token": a.Token,
+	}
+	req := requests.Delete(controlURL(a.Email), bytes.NewReader(lo.Must(json.Marshal(payload))))
+	requests.SetHeaders(req, jsonHeader)
+	_, err := requests.Do(client, req)
 
-    return err
+	return err
 }
 
 type attachmentJson struct {
-    ID         string `json:"id"`
-    HasPreview bool   `json:"has_preview"`
-    Name       string `json:"name"`
-    Size       int    `json:"size"`
+	ID         string `json:"id"`
+	HasPreview bool   `json:"has_preview"`
+	Name       string `json:"name"`
+	Size       int    `json:"size"`
 }
 
 type emailJson struct {
-    ID          string           `json:"id"`
-    From        string           `json:"from"`
-    To          string           `json:"to"`
-    Subject     string           `json:"subject"`
-    BodyText    string           `json:"body_text"`
-    BodyHTML    string           `json:"body_html"`
-    CreatedAt   string           `json:"created_at"`
-    Attachments []attachmentJson `json:"attachments"`
+	ID          string           `json:"id"`
+	From        string           `json:"from"`
+	To          string           `json:"to"`
+	Subject     string           `json:"subject"`
+	BodyText    string           `json:"body_text"`
+	BodyHTML    string           `json:"body_html"`
+	CreatedAt   string           `json:"created_at"`
+	Attachments []attachmentJson `json:"attachments"`
 }
 
 func (a *Account) GetMailbox() ([]Mail, error) {
-    req := requests.Get(mailboxURL(a.Email))
-    body, _, err := requests.DoAndReadString(client, req)
-    if err != nil {
-        log.Println(err)
-        return nil, err
-    }
+	req := requests.Get(mailboxURL(a.Email))
+	body, _, err := requests.DoAndReadString(client, req)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
 
-    var emailJsons []emailJson
-    err = json.Unmarshal([]byte(body), &emailJsons)
-    if err != nil {
-        log.Println(err)
-        return nil, err
-    }
+	var emailJsons []emailJson
+	err = json.Unmarshal([]byte(body), &emailJsons)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
 
-    var mailSlice []Mail
-    for _, eJ := range emailJsons {
-        mail := Mail{
-            ID:          eJ.ID,
-            From:        eJ.From,
-            To:          eJ.To,
-            Subject:     eJ.Subject,
-            BodyText:    eJ.BodyText,
-            BodyHTML:    eJ.BodyHTML,
-            CreatedAt:   eJ.CreatedAt,
-            Attachments: make([]string, 0),
-        }
+	var mailSlice []Mail
+	for _, eJ := range emailJsons {
+		mail := Mail{
+			ID:          eJ.ID,
+			From:        eJ.From,
+			To:          eJ.To,
+			Subject:     eJ.Subject,
+			BodyText:    eJ.BodyText,
+			BodyHTML:    eJ.BodyHTML,
+			CreatedAt:   eJ.CreatedAt,
+			Attachments: make([]map[string]string, 0),
+		}
 
-        if len(eJ.Attachments) > 0 {
-            var attachments []string
-            for _, aJ := range eJ.Attachments {
-                attachments = append(attachments, attachmentURL(aJ.ID))
-            }
-            mail.Attachments = attachments
-        }
+		if len(eJ.Attachments) > 0 {
+			var attachments []map[string]string
+			for _, aJ := range eJ.Attachments {
+				attachments = append(attachments, map[string]string{aJ.Name: attachmentURL(aJ.ID)})
+			}
+			mail.Attachments = attachments
+		}
 
-        mailSlice = append(mailSlice, mail)
-    }
+		mailSlice = append(mailSlice, mail)
+	}
 
-    return mailSlice, nil
+	return mailSlice, nil
 }
--- mail.go
+++ mail.go
@@ -1,12 +1,12 @@
 package tempmail_wrapper
 
 type Mail struct {
-    ID          string
-    From        string
-    To          string
-    Subject     string
-    BodyText    string
-    BodyHTML    string
-    CreatedAt   string
-    Attachments []string
+	ID          string
+	From        string
+	To          string
+	Subject     string
+	BodyText    string
+	BodyHTML    string
+	CreatedAt   string
+	Attachments []map[string]string
 }
